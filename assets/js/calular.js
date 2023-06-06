3bee46f00aee09a97a3d5c28f1559ace7bc5f091
Francisco <franciscomeza.1234@gmail.com>
2023-06-06 00:05:47 -0400

    2do commit

--- assets/js/calular.js
+++ assets/js/calular.js
@@ -2,10 +2,17 @@ function calcular() {
     const valor = 50000;
     const cantidad = parseInt(document.querySelector("#cantidad").value);
     const total = valor * cantidad;
+   
 
     document.querySelector("#totalFinal").textContent = total;
     document.querySelector("#cantidadFinal").textContent = cantidad;
-    
+    if (cantidad<=0) {
+        text ="Porfavor ingresar un número válido"
+        document.querySelector("#totalFinal").innerHTML = text;
+        document.querySelector("#cantidadfinal").style.display = "none";
+    } else {
+        document.querySelector("#totalFinal").innerHTML = "$" + total;
+    }
 }
 function colorChange() {
     var colorSelected, colorBox;
